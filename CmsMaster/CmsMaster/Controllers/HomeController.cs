f29d3d5cbfe50e12c4636849388ac190b74ef60a
Pawel <pawel.klima@webstruments.pl>
2014-06-14 01:26:31 +0200

    Basic
    make basic template with authentication

---
+++ CmsMaster/CmsMaster/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CmsMaster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CmsMaster.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.IsValid(user.UserName, user.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, user.RememberMe);
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Dane niepoprawne");
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ CmsMaster/CmsMaster/Models/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using CmsMaster.Helpers;
+
+namespace CmsMaster.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Pole wymagane")]
+        [Display(Name = "Login")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Pole wymagane")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj")]
+        public bool RememberMe { get; set; }
+
+        public bool IsValid(string _username, string _password)
+        {
+            using (var cn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jezus\Documents\GitHub\Damages\CmsMaster\CmsMaster\App_Data\CmsDatabase.mdf;Integrated Security=True"))
+            {
+                string _sql = @"SELECT [Username] FROM [dbo].[Authentication] " +
+                       @"WHERE [Username] = @u AND [Password] = @p";
+                var cmd = new SqlCommand(_sql, cn);
+                cmd.Parameters
+                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                    .Value = _username;
+                cmd.Parameters
+                    .Add(new SqlParameter("@p", SqlDbType.NVarChar))
+                    .Value = Helpers.SHA1.Encode(_password);
+                cn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return true;
+                }
+                else
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return false;
+                }
+            }
+        }
+    }
+}
